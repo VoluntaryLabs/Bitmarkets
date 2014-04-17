1490c76f6c240a050592f36fba696983c782846c
Steve Dekorte <steve@dekorte.com>
2014-04-17 15:59:51 -0700

    moving in market classes

---
+++ BitMarkets/Classes/Market/MKSells.h
@@ -0,0 +1,16 @@
+//
+//  MKSells.h
+//  Bitmessage
+//
+//  Created by Steve Dekorte on 3/13/14.
+//  Copyright (c) 2014 Bitmarkets.org. All rights reserved.
+//
+
+#import <BitmessageKit/BMNode.h>
+#import "MKSell.h"
+
+@interface MKSells : BMNode
+
+- (MKSell *)justAdd;
+
+@end
