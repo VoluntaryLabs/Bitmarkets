e729f01beecd5c9926cf90c039e0c8c103e61481
Steve Dekorte <steve@dekorte.com>
2014-05-08 15:27:43 -0700

    added MKPost priceInSatoshi method

---
+++ BitMarkets/Classes/Market/Messages/MKConfirmLockEscrowMsg.m
@@ -0,0 +1,13 @@
+//
+//  MKConfirmLockEscrowMsg.m
+//  BitMarkets
+//
+//  Created by Steve Dekorte on 5/8/14.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
+//
+
+#import "MKConfirmLockEscrowMsg.h"
+
+@implementation MKConfirmLockEscrowMsg
+
+@end
--- BitMarkets/Classes/Market/Transactions/Buys/Stages/MKBuyLockEscrow.m
+++ BitMarkets/Classes/Market/Transactions/Buys/Stages/MKBuyLockEscrow.m
@@ -14,6 +14,7 @@
 
 #import "MKRootNode.h"
 #import <BitnashKit/BitnashKit.h>
+#import "MKConfirmLockEscrowMsg.h"
 
 @implementation MKBuyLockEscrow
 
