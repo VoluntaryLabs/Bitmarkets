f5adf0dac6ae4c0d0e9c04b88334ee9e1bb7da78
Rich Collins <richcollins@gmail.com>
2014-05-18 13:49:13 -0700

    MKBuyerCancelLockEscrowMsg

---
+++ BitMarkets/Classes/Market/Transactions/Buys/Stages/Lock/MKBuyerCancelLockEscrowMsg.h
@@ -0,0 +1,13 @@
+//
+//  MKBuyerCancelEscrowMsg.h
+//  BitMarkets
+//
+//  Created by Rich Collins on 5/18/14.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
+//
+
+#import "MKEscrowMsg.h"
+
+@interface MKBuyerCancelLockEscrowMsg : MKEscrowMsg
+
+@end
