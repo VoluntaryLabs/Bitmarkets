6b2a5ec8082502b6b92611f41393dfc8d183ac08
Steve Dekorte <steve@dekorte.com>
2014-10-27 18:18:03 -0700

    latest

---
+++ commit.sh
@@ -0,0 +1,43 @@
+cd submodules
+cd BitmessageKit
+cd submodules
+
+cd FoundationCategoriesKit
+pwd
+git add .
+git commit .
+cd ..
+
+cd NavKit
+pwd
+git add .
+git commit .
+cd ..
+
+cd NavNodeKit
+pwd
+git add .
+git commit .
+cd ..
+
+cd SystemInfoKit
+pwd
+git add .
+git commit .
+cd ..
+
+cd TorServerKit
+pwd
+git add .
+git commit .
+cd ..
+
+cd ..
+pwd
+git add .
+git commit .
+
+cd ../..
+pwd
+git add .
+git commit .
