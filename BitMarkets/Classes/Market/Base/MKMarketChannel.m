5979fea8e44309d57ddb44dee3b27c4397c29f49
Steve Dekorte <steve@dekorte.com>
2014-11-12 15:31:51 -0800

    cleanup

--- BitMarkets/Classes/Market/Base/MKMarketChannel.m
+++ BitMarkets/Classes/Market/Base/MKMarketChannel.m
@@ -3,7 +3,7 @@
 //  Bitmessage
 //
 //  Created by Steve Dekorte on 3/13/14.
-//  Copyright (c) 2014 Bitmarkets.org. All rights reserved.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
 //
 
 #import "MKMarketChannel.h"
